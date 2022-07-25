6d3401c806ca4642ce077c75ca1cc7a13e4f3f10
jpflores-13 <jpflores013@gmail.com>
2022-07-25 11:46:24 -0400

    update man/ and R/

--- R/matches.R
+++ R/matches.R
@@ -1,3 +1,15 @@
+#' Makes a standard dataframe for soccer league data
+#'
+#' @param data_engsoc data_engsoc  obtained from {engsoccerdata}.
+#' @param country `character` scalar, specifies the league.
+#'
+#' @return a tibble with columns `country`, `date`, `season`, `tier`, `home`,
+#'    `visitor`, `goals_home`, `goals_visitor`.
+#' @export
+#'
+#'
+#'@examples
+#'uss_make_matches(engsoccerdata::spain, "Spain")
 uss_make_matches <- function(data_engsoc, country) {
   result <-
     data_engsoc |>
